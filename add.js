d3106085260ff429d057fa852ba79ba140a125d0
Max Yaromchuk <maxmaestro1330@gmail.com>
2019-04-22 15:26:30 +0300

    new commit

---
+++ add.js
@@ -0,0 +1,32 @@
+// for (let list = '#'; list.length < 8 ; list += '#')
+// console.log(list);
+
+
+
+//Fizz Buzz
+// for(let n = 1; n <= 100 ; n++){
+//   let output = "";
+//   if(n % 3 == 0 ) output += 'Tri';
+//   if(n % 5 == 0 ) output += 'Ton';
+//   console.log(output || n);
+  
+// }
+
+//Chess
+// let size = 50;
+
+// let board = '';
+
+// for (let y = 0; y < size; y++){
+//   for (let x = 0; x < size; x++) {
+//     if ((x + y) % 2 === 0){
+//       board += ' ';
+//     }else {
+//       board += '#';
+//     }
+    
+//   }
+//   board += '\n'
+// }
+
+// console.log(board);
